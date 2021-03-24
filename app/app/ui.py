533caff0c85488e01a6eccad9c1eae143ea428a0
Sam Skjonsberg <sams@allenai.org>
2021-03-23 17:15:46 -0700

    Social sharing + tweaks to the add model page.
    This adds social sharing headers (and an accompanying image). I also
    snuck in a few small tweaks to the "Add Project" page in the interest
    of making it look a little better.

--- app/app/ui.py
+++ app/app/ui.py
@@ -6,14 +6,27 @@ from werkzeug.exceptions import NotFound
 def create_ui() -> Blueprint:
     app = Blueprint("app", __name__)
 
+    default_title = "AllenNLP Project Gallery"
+    default_description = (
+        "A list of publicly available, state-of-the-art paper implementations build with "
+        "AllenNLP. The featured projects are developed both by the Allen Institute for AI and "
+        "the larger community."
+    )
+
     @app.route("/")
     def index():
         models = load_all_models()
-        return render_template("index.html", models=models)
+        return render_template("index.html", models=models, title=default_title,
+                                description=default_description)
 
     @app.route("/add-model")
     def add_model():
-        return render_template("add_model.html")
+        title = f"Add Your Project — {default_title}"
+        description = (
+            "We welcome and encourage submissions from the community. Accepted submissions "
+            "are displayed in the Gallery and available for public use."
+        )
+        return render_template("add_model.html", title=title, description=description)
 
     @app.route("/model/<string:model_id>")
     def model_details(model_id: str):
@@ -28,6 +41,11 @@ def create_ui() -> Blueprint:
         if model is None:
             raise NotFound(f"No model with id {model_id}.")
 
-        return render_template("model_details.html", model=model)
+
+        title = f"{model.config.title} — {default_title}"
+        description = model.description
+
+        return render_template("model_details.html", model=model, title=title,
+                                description=description)
 
     return app
